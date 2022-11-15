73c28f7c217baf367abcd1bcdbd7472f0c3034ca
Juliano Falotico <juliano1@live.ca>
2022-11-15 17:11:59 -0500

    Start JS tests

--- updateDesign.js
+++ updateDesign.js
@@ -1,9 +1,9 @@
 // JavaScript source code
 
 function resetClick() {
-
+    document.getElementById("designBox").src = "testChair2.jpg";
 }
 
 function createNewClick() {
-
+    window.location.reload();
 }
