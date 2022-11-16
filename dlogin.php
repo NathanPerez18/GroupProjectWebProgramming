2075a99c4bdf13b94b4aef55edfea99934ecd23a
Honsoloberg <117222684+Honsoloberg@users.noreply.github.com>
2022-11-15 20:24:36 -0500

    Files Converted
    Converted all HTML files to PHP files

---
+++ dlogin.php
@@ -0,0 +1,34 @@
+<?php
+class login extends config{
+		
+		private $uname;
+		private $upass;
+
+		public function login(){
+			$sql = "SELECT uname FROM users WHERE uname LIKE '$uname'";
+			$result = $connection->query($sql);
+			if(mysqli_num_rows($sql) > 0){
+				//exists
+			}else{
+				echo "User doesn't exist";
+				return 0;
+			}
+
+			$result = $connection->query($sql);
+			$row = $result->fetch_assoc();
+			if($row['upass'] == $upass){
+				$_SESSION["uid"] = $row["id"];
+			}
+
+		}
+
+		public function createUser($uname, $upass){
+
+		$sql = "INSERT INTO users (uname, upass) VALUES (
+			'{$connection->real_escape_string($_POST['uname'])}',
+			'{$connection->real_escape_string($_POST['upass'])}',
+			'{$connection->real_escape_string($_POST['email'])}'";
+			$insert = $connection->query($sql);
+		}
+	}
+?>
--- login.php
+++ login.php
@@ -1,34 +1,69 @@
-<?php
-class login extends config{
-		
-		private $uname;
-		private $upass;
+﻿<!DOCTYPE html>
 
-		public function login(){
-			$sql = "SELECT uname FROM users WHERE uname LIKE '$uname'";
-			$result = $connection->query($sql);
-			if(mysqli_num_rows($sql) > 0){
-				//exists
-			}else{
-				echo "User doesn't exist";
-				return 0;
-			}
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>MyKia</title>
 
-			$result = $connection->query($sql);
-			$row = $result->fetch_assoc();
-			if($row['upass'] == $upass){
-				$_SESSION["uid"] = $row["id"];
-			}
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="login.css">
 
-		}
+</head>
+<body>
 
-		public function createUser($uname, $upass){
+    <div class="header">
+        <img src="WebProgrammingLogo.png" alt="MyKia" style="width: 150px; height: 100px;">
+        <h2> MyKia</h2>
+        <h3 style="padding-right: 6px;"> Welcome, User!!</h3>
+    </div>
 
-		$sql = "INSERT INTO users (uname, upass) VALUES (
-			'{$connection->real_escape_string($_POST['uname'])}',
-			'{$connection->real_escape_string($_POST['upass'])}',
-			'{$connection->real_escape_string($_POST['email'])}'";
-			$insert = $connection->query($sql);
-		}
-	}
-?>
+    <div class="navBar">
+        <ul class="navList">
+            <li class="navItems"><a href="index.html">Home</a></li>
+            <li class="navItems"><a href="chair.html">Chair</a></li>
+            <li class="navItems"><a href="table.html">Table</a></li>
+            <li class="navItems"><a href="contact.html">Contact</a></li>
+            <li class="navItems" id="loginBut"><a href="login.html">Login</a></li>
+        </ul>
+    </div>
+
+    <div class="mainContainer">
+
+        <h1 style="text-align: center;">Login</h1>
+
+        <form action="/action_page.php" method="post">
+            <div class="imgContainer">
+                <img src="loginIcon.png" alt="Avatar" class="avatar">
+            </div>
+
+            <div class="bodyContainer">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required>
+
+                <label for="upass"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="upass" required>
+
+                <button type="submit">Login</button>
+                <label>
+                    <input type="checkbox" checked="checked" name="remember"> Remember me
+                </label>
+            </div>
+
+            <div class="bodyContainer" style="background-color: #f1f1f1; border-radius: 2%;">
+                <button type="button" class="cancelbtn">Cancel</button>
+                <span class="psw">Forgot <a href="#">password?</a></span>
+            </div>
+
+            <div class="bodyContainer">
+                <span class="psw" style="padding-top: 3px;">Don't Have an Account? <a href="createAccount.html">Create One!</a></span>
+            </div>
+        </form>
+
+    </div>
+
+    <div class="footerBar">
+        <p>Copyright LRNJ 2022<span>&copy;</span></p>
+    </div>
+
+</body>
+</html>
