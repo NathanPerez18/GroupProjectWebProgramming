8b6d9e7da9a88774469b9b33ae121b8b85cdd32c
NathanPerez18 <45292441+NathanPerez18@users.noreply.github.com>
2022-11-16 11:08:15 -0500

    Create dchair.php
    php for chair

---
+++ dchair.php
@@ -0,0 +1,30 @@
+﻿<?php
+class chair extends config{
+
+
+/*
+Want to store the Primary id of the user, 
+the name of the save, a point on a two d array twice.
+
+Variables:
+primaryKey: Primary id of the user
+nameOfSave: User defined name of the save
+material: Material of the top
+$rowForQueriedData: The data 
+->: Call operator for inherited methods
+
+*/
+	function saveChair(){
+	$sql = "INSERT INTO chair (primaryKey, nameOfSave, material, legsOfChair)";
+	$result = $connection->query($sql);
+	$rowForQueriedData = $result->fetch_assoc();
+	}
+
+	$sql = "INSERT INTO users (uname, upass) VALUES (
+			'{$connection->real_escape_string($_POST['uname'])}',
+			'{$connectino->real_escape_string($_POST['upass'])}'";
+				$insert = $connection->query($sql);
+
+
+}
+?>
