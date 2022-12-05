060ae922f8917e8e832225f25b90c678324244e2
Honsoloberg <117222684+Honsoloberg@users.noreply.github.com>
2022-12-05 13:51:22 -0500

    Comments added
    dtable and dchair now have appropriate comments in short point form

--- dchair.php
+++ dchair.php
@@ -4,21 +4,25 @@ class chair extends config{
     private $saves;
 
     /*
-        pullSaveNames() takes no params. The function, opens a DB connection,
-        runs an SQL query on the table 'chair', then outputs the 
-        correct saved designs from the table and stores them in cookies
-        to later be used with JS
+    All functions in this class create the communication between the chair page and the database
     */
+
+//Pulls the names of saves for the drop down menu of the chair page
     public function pullSaveNames(){
+        //creates a connection to the database
         $connection = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
 
+        //saves the user id store in the session array to a variable to be used in the function
         $id = $_SESSION["uid"];
-        
+
+        //creates the query for the database to return the names of the users saves
         $sql = "SELECT nameOfSave FROM chair WHERE id = '$id'";
         $result = $connection->query($sql);
 
+         //close database connection after the query data is returned
         $connection->close();
 
+        //determine if any saves where returned, if there were then they are saved in the gobal saves array  
         if($result->num_rows == 0){
             $this->saves[0] = "NoSavesFound";
         }else{
@@ -28,33 +32,37 @@ class chair extends config{
                 $check += 1;
             }
         }
+        
+        //All saved saves are put into cookies for JavaScript to process for the page
         for($i = 0; $i< sizeof($this->saves); $i++){
             setcookie("chairCookie".$i,$this->saves[$i],time()+60*5);
         }
     }
 
-    /*
-        fetchSaves() takes no params. The function, opens a DB connection,
-        runs an SQL query on the table 'chair', then outputs the 
-        correct design options that are saved in the table and stores them in cookies
-        to later be used with JS
-    */
+    //Pulls the selected save from the dropdown menu in chair page
     public function fetchSave(){
+        //determine wether the selected option is valid
         if($_POST['savedTable'] != "NoSavesFound" && $_POST['savedTable'] != 'Previous saves'){
+            // save the saveName into a variable
             $saveName = $_POST['savedTable'];
+            
+            //creates a connection to the database
             $connection = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
 
-
+             //saves the user id store in the session array to a variable to be used in the function
 			$id = $_SESSION["uid"];
 
+            //create query to pull selected saves
 			$sql = "SELECT * FROM chair WHERE nameOfSave = '$saveName' AND id = '$id'";
             $result = $connection->query($sql);
 
+            //close database connection after the query data is returned
             $connection->close();
 
+             //transform the returned query data into a usable form
             $formContent = $result->fetch_assoc();
          
-
+            //set returned data into corresponding cookies
             setcookie('formContentChair1', $formContent['material'] , time()+30);
             setcookie('formContentChair2', $formContent['materialColor'] , time()+30);
             setcookie('formContentChair3', $formContent['legs'] , time()+30);
@@ -62,17 +70,18 @@ class chair extends config{
         }
     }
 
-    /*
-        createSave() takes no params. The function, opens a DB connection,
-        runs an SQL query on the table 'chair' to insert the relevant information 
-        into the 'chair' table that contains the; user id, save name, an int for material,
-        an int for upholstery color, and int for leg shape, and an int for leg color.
-    */
+    // Creates a user save
     public function createSave(){
+        //checkes if the user has logged in else returns and alert to log in
         if(!empty($_SESSION["uid"])){
+
+            //creates a connection to the database
             $connection = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+
+            //pulls the saveName set by the user
             $name = $_COOKIE['saveCookie'];
 
+            //query String to create the save for the user
             $sql = "INSERT INTO chair (id, nameOfSave, material, materialColor, legs, legColor) VALUES (
                 '{$connection->real_escape_string($_SESSION['uid'])}',
                 '{$connection->real_escape_string($name)}',
@@ -80,11 +89,14 @@ class chair extends config{
                 '{$connection->real_escape_string($_POST['colorUpholstery'])}',
                 '{$connection->real_escape_string($_POST['legs'])}',
                 '{$connection->real_escape_string($_POST['colorLegs'])}')";
-            
+
+            //close the database connection
             $connection->query($sql);
 
+            //close database connection after the query data is returned
             $connection->close();
 
+            //reloads the page to prevent form resubmission
             header("Location: chair.php");
         }else {
             echo "<script>
--- dtable.php
+++ dtable.php
@@ -2,24 +2,28 @@
 include 'config.php';
 class dtable extends config{
 
+    /*
+    All functions in this class create the communication between the table page and the database
+    */
+
     private $saves;
 
-      /*
-        pullSaveNames() takes no params. The function, opens a DB connection,
-        runs an SQL query on the table 'tableoftables', then outputs the 
-        correct saved designs from the table and stores them in cookies
-        to later be used with JS
-    */
+    //Pulls the names of saves for the drop down menu of the table page
     public function pullSaveNames(){
+        //creates a connection to the database
         $connection = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
 
+        //saves the user id store in the session array to a variable to be used in the function
         $id = $_SESSION["uid"];
         
+        //creates the query for the database to return the names of the users saves
         $sql = "SELECT nameOfSave FROM tableoftables WHERE id = '$id'";
         $result = $connection->query($sql);
 
+        //close database connection after the query data is returned
         $connection->close();
-        
+
+        //determine if any saves where returned, if there were then they are saved in the gobal saves array   
         if($result->num_rows == 0){
             $this->saves[0] = "NoSavesFound";
         }else{
@@ -29,47 +33,55 @@ class dtable extends config{
                 $check += 1;
             }
         }
+
+        //All saved saves are put into cookies for JavaScript to process for the page
         for($i = 0; $i< sizeof($this->saves); $i++){
             setcookie("tableCookie".$i,$this->saves[$i],time()+5);
         }
     }
 
-      /*
-        fetchSaves() takes no params. The function, opens a DB connection,
-        runs an SQL query on the table 'tablesoftables', then outputs the 
-        correct design options that are saved in the table and stores them in cookies
-        to later be used with JS
-    */
+    //Pulls the selected save from the dropdown menu in table page
     public function fetchSave(){
+        //determine wether the selected option is valid
         if($_POST['savedTable'] != "NoSavesFound" && $_POST['savedTable'] != 'Previous saves'){
+            // save the saveName into a variable
             $saveName = $_POST['savedTable'];
+
+            //creates a database connection
             $connection = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+            
+            //saves the user id store in the session array to a variable to be used in the function
             $id = $_SESSION["uid"];
 
+            //create query to pull selected saves
             $sql = "SELECT * FROM tableoftables WHERE nameOfSave = '$saveName' AND id = '$id'";
             $result = $connection->query($sql);
 
+            //close database connection after query data is returned
             $connection->close();
 
+            //transform the returned query data into a usable form
             $formContent = $result->fetch_assoc();
 
+            //set returned data into corresponding cookies
             setcookie('formContentTable1', $formContent['top'] , time()+30);
             setcookie('formContentTable2', $formContent['topColor'] , time()+30);
             setcookie('formContentTable3', $formContent['legs'] , time()+30);
             setcookie('formContentTable4', $formContent['legColor'] , time()+30);
         }
     }
-    /*
-        createSave() takes no params. The function, opens a DB connection,
-        runs an SQL query on the table 'tableoftables' to insert the relevant information 
-        into the 'chair' table that contains the; user id, save name, an int for tabletop type,
-        an int for tabletop color, and int for leg shape, and an int for leg color.
-    */
+
+    // Creates a user save
     public function createSave(){
+        //checkes if the user has logged in else returns and alert to log in
         if(!empty($_SESSION["uid"])){
+            //creates database connection
             $connection = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+
+            //pulls the saveName set by the user
             $name = $_COOKIE['saveCookie'];
 
+            //query String to create the save for the user
             $sql = "INSERT INTO tableoftables (id, nameOfSave, top, topColor, legs, legColor) VALUES (
                 '{$connection->real_escape_string($_SESSION['uid'])}',
                 '{$connection->real_escape_string($name)}',
@@ -80,8 +92,10 @@ class dtable extends config{
             
             $connection->query($sql);
 
+            //close the database connection
             $connection->close();
 
+            //reloads the page to prevent form resubmission
             header("Location: table.php");
         }else {
             echo "<script>
